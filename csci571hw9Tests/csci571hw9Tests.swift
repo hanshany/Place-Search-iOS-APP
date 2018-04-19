976554952d79c6352b747b911d199a8df001baf8
叶晗姗 <walffymmd@gmail.com>
2018-04-19 14:45:50 -0700

    Initial Commit

---
+++ csci571hw9Tests/csci571hw9Tests.swift
@@ -0,0 +1,36 @@
+//
+//  csci571hw9Tests.swift
+//  csci571hw9Tests
+//
+//  Created by Hanshan Ye on 4/19/18.
+//  Copyright © 2018 Hanshan Ye. All rights reserved.
+//
+
+import XCTest
+@testable import csci571hw9
+
+class csci571hw9Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
